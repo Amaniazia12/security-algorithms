5458f1a78e6cbad5aeabe3326ff6f9d737a3ccec
mennashrif <mennashrif2000@gmail.com>
2021-05-10 18:08:12 +0200

    all algorithms with some proplems in columnar

--- SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,17 +10,188 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
+
+
+            int[] xx = new int[] { 1 };
+            for (int i = 2; i < plainText.Length; i++)
+            {
+                var resulttt = new List<IList<int>>();
+
+                xx = xx.Concat(new int[] { i }).ToArray();
+
+                resulttt = DoPermute(xx, 0, xx.Length - 1, resulttt);
+                //Console.WriteLine(resulttt.Count+ "   22");
+                //Console.WriteLine($"    [{string.Join(',', resulttt)}]");
+                foreach (List<int> resultt in resulttt)
+                {
+                    // Console.WriteLine($"    [{string.Join(',', resultt)}]");
+                    string s = Encrypt(plainText, resultt);
+
+                    Console.WriteLine(s);
+                    //Console.WriteLine($"    [{string.Join(',', resultt)}]");
+                    //Console.WriteLine(" 11 ");
+                    if (s.Equals(cipherText.ToUpper()))
+                    {
+                        for (int k = 0; k < resultt.Count; k++)
+                        {
+                            resultt[k]++;
+
+                        }
+                        //Console.WriteLine($"    [{string.Join(',', resultt)}]");
+
+                        return resultt;
+                    }
+
+                }
+
+            }
+            return new List<int>();
             throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, List<int> key)
         {
+            cipherText = cipherText.ToLower();
+            int max = 0;
+
+            for (int i = 0; i < key.Count; i++)
+            {
+                key[i]--;
+                if (max < key[i])
+                    max = key[i];
+            }
+            int row = cipherText.Length / (max + 1);
+            int x = 0;
+            /**
+            if (row * (max + 1) < cipherText.Length)
+            {
+                row++;
+                x = row * (max + 1) - cipherText.Length;
+
+            }
+            **/
+            //Console.WriteLine(row + " , " + max + "," + cipherText.Length + "," + x);
+            string[,] mat = new string[20, max + 1];
+            string outp = "";
+            int indx = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                int y = key.IndexOf(i);
+                //Console.WriteLine(y);
+                for (int j = 0; j < row; j++)
+                {
+                    if (indx == cipherText.Length)
+                    { mat[j, y] = "x"; }
+
+                    /** if (j == row - 1 && i >= (max - x + 1))
+                     {
+                         mat[j, y] = " ";
+                     }**/
+                    else
+                    {
+                        mat[j, y] = cipherText[indx].ToString(); indx++;
+                        // Console.WriteLine(indx + " index");
+                    }
+
+                    Console.WriteLine(mat[j, y].ToString() + ",");
+
+                }
+                Console.WriteLine(outp);
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < max + 1; j++)
+                {
+                    outp += mat[i, j].ToString();
+                    Console.Write(mat[i, j].ToString() + ",");
+                }
+                Console.WriteLine(" ");
+            }
+
+            Console.WriteLine(outp);
+            return outp;
             throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
+
+            plainText = plainText.ToLower();
+            int max = 0;
+            for (int i = 0; i < key.Count; i++)
+            {
+                key[i]--;
+                if (max < key[i])
+                    max = key[i];
+            }
+            //Console.WriteLine(max);
+            string[,] mat = new string[20, max + 1];
+            int indx = 0;
+            int row = 0;
+            string outp = "";
+            for (int i = 0; indx < plainText.Length; i++)
+            {
+                for (int j = 0; j <= max && indx < plainText.Length; j++)
+                {
+                    mat[i, j] = plainText[indx].ToString();
+                    indx++;
+                    //Console.Write(mat[i, j] + ",");
+                }
+                row++;
+                //Console.WriteLine(" ");
+            }
+            for (int i = 0; i < max + 1; i++)
+            {
+                int y = key.IndexOf(i);
+                //Console.WriteLine(y);
+                for (int j = 0; j < row; j++)
+                {
+                    if (!(mat[j, y] == " "))
+                        outp += mat[j, y];
+                    else
+                    {
+                        outp += 'x';
+                    }
+                }
+                // Console.WriteLine(outp);
+            }
+            // Console.WriteLine(outp);
+            return outp.ToUpper();
             throw new NotImplementedException();
         }
+        public List<IList<int>> DoPermute(int[] nums, int start, int end, List<IList<int>> list)
+        {
+
+            if (start == end)
+            {
+                // We have one of our possible n! solutions,
+                // add it to the list.
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    //Console.WriteLine( nums[start] + " ," +  nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    //Console.WriteLine($"    [{string.Join(',', nums)}]");
+                    Swap(ref nums[start], ref nums[i]);
+                    //Console.WriteLine(nums[start] + " ,," + nums[i]);
+                }
+            }
+            //PrintResult(list);
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+
     }
 }
+
--- SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -10,17 +10,168 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            char[,] matr = Matricx2D();
+            int jj = 0;
+            string outp = "";
+            int x = 0, y = 0;
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                //int x = 0, y = 0;
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[0, j].Equals(plainText[i]))
+                    {
+                        x = j;
+                        break;
+                    }
+                }
+                //Console.WriteLine("11");
+
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[j, x].Equals(cipherText[i]))
+                    {
+                        y = j;
+                        outp += matr[y, 0];
+                        break;
+                    }
+                }
+            }
+            int index=0;
+            for(int i = outp.Length-1; i > 0; i--)
+            {
+     
+                string C = Encrypt(plainText, outp.Substring(0, i));
+                if (C.Equals(cipherText.ToUpper()))
+                {
+                    index = i;
+                }
+            }
+      
+            Console.WriteLine(outp.Substring(0, index));
+            return outp.Substring(0, index);
+           
         }
 
         public string Decrypt(string cipherText, string key)
         {
+            cipherText = cipherText.ToLower();
+            char[,] matr = Matricx2D();
+            int len = 0;
+            string str = key;
+            string outp = "";
+            int xx = 0;
+            for (int i = 0; str.Length < cipherText.Length; i++, xx++)
+            {
+                if (xx == key.Length)
+                {
+                    xx = 0;
+                }
+                str += key[xx].ToString();
+            }
+            Console.WriteLine(str);
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int x = 0, y = 0;
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[0, j].Equals(str[i]))
+                    {
+                        x = j;
+                        break;
+                    }
+                }
+                //Console.WriteLine("11");
+
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[j, x].Equals(cipherText[i]))
+                    {
+                        y = j;
+                        outp += matr[y, 0];
+                        break;
+                    }
+                }
+                //Console.WriteLine("22" );
+
+                Console.WriteLine(outp);
+            }
+
+            return outp.ToUpper();
             throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, string key)
         {
+            plainText = plainText.ToLower();
+            char[,] matr = Matricx2D();
+            int len = 0;
+            string str = key;
+            string outp = "";
+            int xx = 0;
+            for (int i = 0; str.Length < plainText.Length; i++, xx++)
+            {
+                if (xx == key.Length)
+                {
+                    xx = 0;
+                }
+                str += key[xx].ToString();
+            }
+            //Console.WriteLine(str);
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int x = 0, y = 0;
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[0, j].Equals(str[i]))
+                    {
+                        x = j;
+                        break;
+                    }
+                }
+                //Console.WriteLine("11");
+
+                for (int j = 0; j < 26; j++)
+                {
+                    if (matr[j, 0].Equals(plainText[i]))
+                    {
+                        y = j;
+                        break;
+                    }
+                }
+                //Console.WriteLine("22" );
+                outp += matr[x, y];
+                //Console.WriteLine(outp);
+            }
+
+            return outp.ToUpper();
             throw new NotImplementedException();
+        }
+
+        public char[,] Matricx2D()
+        {
+            char[,] matrix = new char[26, 26];
+            int x_constatnt = 97;
+            int x = 97;
+            for (int i = 0; i < 26; i++)
+            {
+                x = x_constatnt;
+                for (int j = 0; j < 26; j++)
+                {
+                    if (x == 123)
+                    {
+                        x = 97;
+                    }
+                    matrix[i, j] = (char)x;
+                    x++;
+                    //Console.Write(matrix[i, j] + " , ");
+                }
+                x_constatnt++;
+                //Console.WriteLine("\n");
+            }
+            return matrix;
         }
     }
 }
